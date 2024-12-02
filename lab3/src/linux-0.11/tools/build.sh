02311169278673d1747904e6d5a2fddb70ea5b70
Hongyi Liu <cshyliu@gmail.com>
2024-12-02 19:07:14 +0800

    fix: remove the system module build in Makefile and build.sh

--- lab3/src/linux-0.11/tools/build.sh
+++ lab3/src/linux-0.11/tools/build.sh
@@ -11,7 +11,7 @@ root_dev=$5
 
 # Set the biggest sys_size
 # Changes from 0x20000 to 0x30000 by tigercn to avoid oversized code.
-SYS_SIZE=$((0x3000*16))
+# SYS_SIZE=$((0x3000*16))
 
 # set the default "device" file for root image file
 if [ -z "$root_dev" ]; then
@@ -31,10 +31,10 @@ dd if=$bootsect bs=512 count=1 of=$IMAGE 2>&1 >/dev/null
 dd if=$setup seek=1 bs=512 count=4 of=$IMAGE 2>&1 >/dev/null
 
 # Write system(< SYS_SIZE) to stdout
-[ ! -f "$system" ] && echo "there is no system binary file there" && exit -1
-system_size=`wc -c $system |cut -d" " -f1`
-[ $system_size -gt $SYS_SIZE ] && echo "the system binary is too big" && exit -1
-dd if=$system seek=5 bs=512 count=$((2888-1-4)) of=$IMAGE 2>&1 >/dev/null
+# [ ! -f "$system" ] && echo "there is no system binary file there" && exit -1
+# system_size=`wc -c $system |cut -d" " -f1`
+# [ $system_size -gt $SYS_SIZE ] && echo "the system binary is too big" && exit -1
+# dd if=$system seek=5 bs=512 count=$((2888-1-4)) of=$IMAGE 2>&1 >/dev/null
 
 # Set "device" for the root image file
 echo -ne "\x$DEFAULT_MINOR_ROOT\x$DEFAULT_MAJOR_ROOT" | dd ibs=1 obs=1 count=2 seek=508 of=$IMAGE conv=notrunc  2>&1 >/dev/null
